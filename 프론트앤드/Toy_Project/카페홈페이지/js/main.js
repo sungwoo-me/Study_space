9d1ee88a6861b9c9d00a001f5c9b368de3d43126
2016112546 <awhtjddn1@naver.com>
2022-08-18 13:35:25 +0900

    배지 작업

--- 프론트앤드/Toy_Project/카페홈페이지/js/main.js
+++ 프론트앤드/Toy_Project/카페홈페이지/js/main.js
@@ -16,3 +16,26 @@ searchinputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     searchinputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+    console.log(window.scrollY);
+    if (window.scrollY > 500){
+        // 배지 숨기기 
+        // badgeEl.style.display = 'none' ;
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity : 0 
+        });
+    } else {
+        // 배지 보이기
+        // badgeEl.style.display = 'block';
+        gsap.to(badgeEl, .6, {
+            opacity :1 
+
+        });
+    }
+}, 300));
+
+// _.throttle(함수, 시간)
